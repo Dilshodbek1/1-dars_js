c097f4d1845724676736797a46beadc095ff6ee0
Dilshodbek1 <dilshodprogrammer6636@gmail.com>
2021-08-28 06:02:08 +0500

    js dars 1

---
+++ app.js
@@ -0,0 +1,89 @@
+let number =5;
+console.log(number);
+
+number =14;
+console.log(number);
+
+const pi=3.14;
+
+
+
+// keyingi turi var 
+
+
+// console.log(number2);
+var number2=6;
+number2=10;
+console.log(number2);
+
+
+
+// Malumotlar turlar data type
+
+ const number =5;  //Data type=number
+ const firstname='Dilshod'; //data type string
+
+// true false ya'ni boolen  
+
+ const bool =false; //data-type='boolean'
+
+ const isMarried=true;
+
+
+
+
+
+
+
+
+
+
+
+//  Null undefined
+
+// undefined
+let lastname;
+console.log(lastname);
+
+// null
+console.log(a);
+
+// infinity
+console.log(5/0);
+
+
+// NaN
+console.log('samar'*10);
+
+
+
+
+
+// objectlar
+
+
+const person= {
+    firstname: "dishqa",
+    lastname: "abdullayev",
+    age:19,
+    job: "maxram",
+};
+console.log(person);
+
+
+
+console.log(person.lastname);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
